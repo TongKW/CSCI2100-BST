9b54d6eb9f86fcbcff71316edb0b3d6ad69f4dc5
petertong32 <peter@savvyapp.cc>
2021-12-18 15:42:41 +0800

    fstream

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "bst.h"
 #include "merge_sort.h"
 using namespace std;
 
 int main()
 {
-    
+    string line;
+    ifstream ds_file("input/ds.txt");
+    ifstream qry_file("input/qry.txt");
+    ofstream output_file ("output/output.txt");
+
+    if (ds_file.is_open())
+    {
+        while ( getline (myfile,line) )
+        {
+        cout << line << '\n';
+        }
+        ds_file.close();
+    }
 }
